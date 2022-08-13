f10de32301026929f74ff9f41e69235b9bf711bc
Jvictorcam <jvictorac@proton.me>
2022-08-13 02:08:45 -0300

    Finished initial functions and solved more bugs

--- login.hpp
+++ login.hpp
@@ -10,16 +10,21 @@ char repss[50];
 //end of Variables
 
 void clear(){
-    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
 void readUser(){
-    fflush(stdin);
+    fflush(stdout);
     fgets(user, sizeof(user), stdin);
+    size_t lenu = strlen(user) - 1;
+    if(user[lenu] == '\n') user[lenu] = '\0';
 }
 
 void readPassword(){
+    fflush(stdout);
     fgets(password, sizeof(password), stdin);
+    size_t lenp = strlen(password) - 1;
+    if(password[lenp] == '\n') password[lenp] = '\0';
 }
 
 int Welcome(){
@@ -32,15 +37,29 @@ int Welcome(){
          << "(2): Register\n"
          << "(3): Exit\n"
          << "(4): About Us\n"
-         << '-';
-    cin >> aux;
+         << '-' << '\n'
+         << "-> ";
+    cin >> aux; // say what the user want to do
+    
+    fflush(stdout); // SAFETY
+    fflush(stdin);  // JUST FOR SAFE!!!! HATE GARBAGE, ITS 1AM AND IM CODING THIS
+    cin.ignore();   // SAFETY
+
     return aux;
 }
 
 bool Login(){
-    
+   int attempt = 3;
+   B: 
+    cout << "Sign-In Space\n-\n-\n-\n-\n" 
+         << "User: ";
     readUser();
-    readPassword();
+    cout << "Password: ";
+    readPassword(); 
+    
+   
+    
+    
 
     string userit;
     string passwordit;
@@ -48,8 +67,10 @@ bool Login(){
     file.open("data.txt", ios::app);
     file.close();
     file.open("data.txt", ios::in);
-    if(!file.is_open()){
-        cerr << "Data Error";
+    
+    if(!file.is_open()){        // If the file cant be open or created, then
+        cerr << "An Data Error Ocurred, Sorry\n";   // print "An Data Error Ocurred, Sorry" and
+        return -1;              // return -1
     }
 
     while(!file.eof()){
@@ -59,10 +80,20 @@ bool Login(){
         getline(file, userit);
         getline(file, passwordit);
         if(userit == user && passwordit == password){
+            clear();
+            cout << "Logged Successfully!!\nWelcome to Carot >" << user << "<\n\n\n\n";
+            file.close();
             return true;
         }
     }
     file.close();
+    attempt--;
+    cout << "Authentication Failed\n_!_" << attempt << "Attempts Left _!_\nPress ENTER to continue... ";
+    getchar();
+    clear();
+    
+    if(attempt != 0) goto B;
+
     return false;
 }
 
@@ -70,15 +101,19 @@ void Register(){
     
     cout << "Choose your Username: ";
     readUser();
-    cout << user << "? Okay, its avaible\n";
+
     A:
-    cout << "\nSet a Password: ";
+    cout << "Set a Password: ";
     readPassword();
-    fflush(stdin);
+    fflush(stdout);
     cout << "Re-Write the Password: ";
+
     fgets(repss, sizeof(repss), stdin);
-    fflush(stdin);
-    if(repss != password){
+    size_t lenp2 = strlen(repss) - 1;
+    if(repss[lenp2] == '\n') repss[lenp2] = '\0';
+ 
+
+    if(string(repss) != string(password)){
         cout << "\n\n_!_:The passwords filds doesn't match. Press ENTER to Try Again\n";
         getchar();
         clear();
@@ -96,5 +131,5 @@ void Register(){
     getchar();
     clear();
 
-    fflush(stdin);
+    fflush(stdout);
 }

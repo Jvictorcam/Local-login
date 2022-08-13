3aa1ba627718c53865fd92638c0d41411db6cc44
Jvictorcam <jvictorac@proton.me>
2022-08-12 21:59:15 -0300

    Solved some Bugs and implement more functions, but more problems appeared

--- login.hpp
+++ login.hpp
@@ -1,13 +1,20 @@
 #include <fstream>
 #include <bits/stdc++.h>
+#include <locale.h>
 using namespace std;
 
 //Variables:
 char user[50];
 char password[50];
+char repss[50];
 //end of Variables
 
+void clear(){
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+}
+
 void readUser(){
+    fflush(stdin);
     fgets(user, sizeof(user), stdin);
 }
 
@@ -15,10 +22,31 @@ void readPassword(){
     fgets(password, sizeof(password), stdin);
 }
 
-bool checkLogin(){
+int Welcome(){
+    clear();
+    int aux;
+    cout << "Welcome to Project Carot!";
+    cout << "Select the service u want: "
+         << "\n-\n"
+         << "(1): Login\n"
+         << "(2): Register\n"
+         << "(3): Exit\n"
+         << "(4): About Us\n"
+         << '-';
+    cin >> aux;
+    return aux;
+}
+
+bool Login(){
+    
+    readUser();
+    readPassword();
+
     string userit;
     string passwordit;
     fstream file;
+    file.open("data.txt", ios::app);
+    file.close();
     file.open("data.txt", ios::in);
     if(!file.is_open()){
         cerr << "Data Error";
@@ -34,7 +62,39 @@ bool checkLogin(){
             return true;
         }
     }
+    file.close();
     return false;
 }
 
+void Register(){
+    
+    cout << "Choose your Username: ";
+    readUser();
+    cout << user << "? Okay, its avaible\n";
+    A:
+    cout << "\nSet a Password: ";
+    readPassword();
+    fflush(stdin);
+    cout << "Re-Write the Password: ";
+    fgets(repss, sizeof(repss), stdin);
+    fflush(stdin);
+    if(repss != password){
+        cout << "\n\n_!_:The passwords filds doesn't match. Press ENTER to Try Again\n";
+        getchar();
+        clear();
 
+        goto A;
+    }
+    else
+    cout << "You has been registered.\nPress Enter to Continue to the Login Page... ";
+   
+    ofstream ofile;
+    ofile.open("data.txt", ios::app | ios::out);
+    ofile << user << '\n';
+    ofile << password << '\n';
+
+    getchar();
+    clear();
+
+    fflush(stdin);
+}
--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 using namespace std;
 
 int main(int argc, char** argv){
+    
+    int act = Welcome();
+    bool auth; // save if credentials are valid or not
+    if(act == 1){
+       auth = Login(); // Check the credentials and return if its correct or not
+    }
 
-    readUser();
-    readPassword();
+    if(act == 2){
+        Register();
+        auth = Login();
+    }
 
-    checkLogin();
+    if(!auth){
+        cout << "Access Denied" << endl;
+    }
 
     return 0;
 }
